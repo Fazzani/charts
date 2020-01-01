7275d6aacbb973e464db52e8972e592767de4114
Fazzani <fazzani.heni@outlook.fr>
2020-01-01 19:53:48 +0100

    bump version 0.1.4

--- ci/bumpversion.sh
+++ ci/bumpversion.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-git tag -a 0.1.2 -m 'Version 0.1.2'
+[ -z "$1" ] && echo "Version Semver not supplied !!" && exit 1
+
+git add . && git commit -m "bump version $1" && \
+git tag -a "$1" -m "Version $2" && \
 git push && git push --tags
