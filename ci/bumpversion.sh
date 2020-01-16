2523c5187c9e908bc8ba6fedf0f8dc6de053abba
Fazzani <fazzani.heni@outlook.fr>
2020-01-16 23:58:14 +0100

    bump version 0.1.6

--- ci/bumpversion.sh
+++ ci/bumpversion.sh
@@ -5,4 +5,4 @@
 git add . && git commit -m "bump version $1"
 (git tag -d "$1" && git push origin --delete "$1" || echo -e "$1 tag not exist yet") && \
 git tag -fa "$1" -m "Version $2" &&  \
-git push --tags
+git push --tags && git push
